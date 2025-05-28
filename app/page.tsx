72d0fd88027b4a76a43a2d8e3c1a91abbe2fb6c4
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-05-28 20:28:47 +0100

    Implement Companion Form, Auth, Billing, Supabase Setup and Companion Server Actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to LMS SAAS</h1>
-      <Button variant={"default"}>Get started</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+        <section className={"home-section"}>
+            <CompanionCard
+                id={12}
+                name={"Neura the Brainy Explorer"}
+                topic={"Neural Network of the Brain"}
+                subject={"science"}
+                duration={45}
+                color={"#ffda6e"}
+            />
+            <CompanionCard
+                id={14}
+                name={"Councy the Number Wizard"}
+                topic={"Derivatives and Integrals"}
+                subject={"math"}
+                duration={32}
+                color={"#FF6B6B"}
+            />
+            <CompanionCard
+                id={1432}
+                name={"Verba the Vocab Builder"}
+                topic={"Language Mastery"}
+                subject={"english literature"}
+                duration={12}
+                color={"#bde7af"}
+            />
+        </section>
+        <section className={"home-section"}>
+            <CompanionsList
+                title={"Recently Completed Sessions"}
+                companions={recentSessions}
+                classNames={"w-2/3 max-lg:w-full"}
+            />
+            <CTA/>
+        </section>
+    </main>
+
   )
 }
 
